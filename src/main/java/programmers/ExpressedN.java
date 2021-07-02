e0c251d92fe23e7612d23f0d4bb605f12f153dd8
jojojohhh <qwhtkdwp@gmail.com>
2021-07-02 17:31:54 +0900

    2021/07/02

---
+++ src/main/java/programmers/ExpressedN.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.*;
+
+public class ExpressedN {
+    public int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
