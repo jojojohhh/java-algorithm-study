e019debe53a12d70d91cd701caaeb698ef589422
jojojohhh <qwhtkdwp@gmail.com>
2021-08-02 21:06:12 +0900

    2021/08/02

--- src/main/java/programmers/CalcInsufficientAmount.java
+++ src/main/java/programmers/CalcInsufficientAmount.java
@@ -1,21 +1,8 @@
 package programmers;
 
 public class CalcInsufficientAmount {
-    public long solution(int price, int money, int count) {
-        Long sum = count % 2 == 0 ? (long) (((count + 1) * (count / 2))) * price : (long) ((count / 2) * ((count + 1) + 1) + 1) * price;
+    public long solution(long price, long money, long count) {
+        long sum = (price * count * (count + 1)) / 2;
         return money - sum < 0 ? sum - money : 0;
-    }
-    public static void main(String[] args) {
-        CalcInsufficientAmount c = new CalcInsufficientAmount();
-        System.out.println(c.solution(2500, 1000000000, 2500));
-        System.out.println(c.solution(3, 20, 4));
-//        long sum = 0L;
-//        for (int i = 1; i <= 2500; i++) sum += 2500 * i;
-//        System.out.println(sum);
-//
-//        int a = 1000000000;
-//        System.out.println(a);
-//
-//        System.out.println(a < sum);
     }
 }
