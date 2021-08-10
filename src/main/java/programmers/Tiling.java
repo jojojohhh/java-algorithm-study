f5aa05c13d717b79a247a0edf05088f05202ac48
jojojohhh <qwhtkdwp@gmail.com>
2021-08-11 02:19:40 +0900

    2021/08/11

--- src/main/java/programmers/Tiling.java
+++ src/main/java/programmers/Tiling.java
@@ -2,7 +2,14 @@ package programmers;
 
 public class Tiling {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 1; i < n; i++) {
+            c = (a + b) % 1000000007;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 }
