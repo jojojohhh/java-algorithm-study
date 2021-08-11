79cc4da194591f80c44272a8c23b8bfa1c8f767c
jojojohhh <qwhtkdwp@gmail.com>
2021-08-12 04:28:30 +0900

    2021/08/12

--- src/main/java/programmers/BlastBalloon.java
+++ src/main/java/programmers/BlastBalloon.java
@@ -2,7 +2,19 @@ package programmers;
 
 public class BlastBalloon {
     public int solution(int[] a) {
-        int answer = 0;
-        return answer;
+        int answer = 2;
+        int l = a[0];
+        int r = a[a.length - 1];
+        for (int i = 1; i <a.length - 1; i++) {
+            if (l > a[i]) {
+                l = a[i];
+                answer++;
+            }
+            if (r > a[a.length - 1 - i]) {
+                r = a[a.length - 1 - i];
+                answer++;
+            }
+        }
+        return l == r ? --answer : answer;
     }
 }
