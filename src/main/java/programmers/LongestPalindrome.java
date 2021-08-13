65c50669e35f4f008505818db68e8969841447e5
jojojohhh <qwhtkdwp@gmail.com>
2021-08-13 18:44:09 +0900

    2021/08/13

--- src/main/java/programmers/LongestPalindrome.java
+++ src/main/java/programmers/LongestPalindrome.java
@@ -2,11 +2,18 @@ package programmers;
 
 public class LongestPalindrome {
     public int solution(String s) {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        for (int i = s.length(); i > 1; i--) {
+            for (int j = 0; j <= s.length() - i; j++) {
+                boolean isPalindrome = true;
+                for (int k = 0; k < i / 2; k++) {
+                    if (s.charAt(j + k) != s.charAt(i + j - k - 1)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome)   return i;
+            }
+        }
+        return 1;
     }
 }
