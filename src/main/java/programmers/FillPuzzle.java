dc855699c7ec71204b465615198a2de422fc3f10
jojojohhh <qwhtkdwp@gmail.com>
2021-09-09 19:31:45 +0900

    2021/09/09

--- src/main/java/programmers/FillPuzzle.java
+++ src/main/java/programmers/FillPuzzle.java
@@ -35,3 +35,62 @@ public class FillPuzzle {
     public static void main(String[] args) {
     }
 }
+
+//    public boolean solution(int[][] key, int[][] lock) {
+//        int[][] newLock = new int[(key.length - 1) * 2 + lock.length][(key.length - 1) * 2 + lock.length];
+//        HashSet<String> set = new HashSet<>();
+//
+//        for (int i = 0; i < lock.length; i++) {
+//            for (int j = 0; j < lock.length; j++) {
+//                newLock[key.length - 1 + i][key.length - 1 + j] = lock[i][j];
+//                if (lock[i][j] == 0) {
+//                    set.add(String.valueOf(key.length - 1 + i) + String.valueOf(key.length - 1 + j));
+//                }
+//            }
+//        }
+//
+//        int startLockIdx = key.length - 1;
+//        int endLockIdx = startLockIdx + lock.length - 1;
+//
+//        int rotateCnt = 3;
+//
+//        while (rotateCnt --> 0) {
+//            for (int i = 0; i <= newLock.length - key.length; i++) {
+//                for (int j = 0; j <= newLock.length - key.length; j++) {
+//                    HashSet<String> newSet = new HashSet<>(set);
+//                    boolean fail = false;
+//                    for (int k = 0; k < key.length; k++) {
+//                        if (fail) break;
+//                        for (int l = 0; l < key.length; l++) {
+//                            if ((k + i >= startLockIdx && k + i <= endLockIdx) && (l + j >= startLockIdx && l + j <= endLockIdx)) {
+//                                String val = String.valueOf(k + i) + String.valueOf(l + j);
+//                                if (key[k][l] == 1) {
+//                                    if (newSet.contains(val)) {
+//                                        newSet.remove(val);
+//                                    } else {
+//                                        fail = true;
+//                                        break;
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                    if (newSet.size() == 0) return true;
+//                }
+//            }
+//            key = rotate(key);
+//        }
+//
+//        return false;
+//    }
+//
+//    public int[][] rotate(int[][] key) {
+//        int[][] copyKey = new int[key.length][key.length];
+//
+//        for (int i = 0; i < key.length; i++) {
+//            for (int j = 0; j < key.length; j++) {
+//                copyKey[i][j] = key[key.length - 1 - j][i];
+//            }
+//        }
+//        return copyKey;
+//    }
--- src/main/java/programmers/LockAndKey.java
+++ src/main/java/programmers/LockAndKey.java
@@ -2,7 +2,63 @@ package programmers;
 
 public class LockAndKey {
     public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
-        return answer;
+        int startLockIdx = key.length - 1;
+        int endLockIdx = startLockIdx + lock.length - 1;
+
+        int lockCnt = 0;
+        for (int i = 0; i < lock.length; i++) {
+            for (int j = 0; j < lock.length; j++) {
+                if (lock[i][j] == 0) lockCnt++;
+            }
+        }
+
+        int[][] newLock = new int[startLockIdx * 2 + lock.length][startLockIdx * 2 + lock.length];
+
+        for (int i = 0; i < lock.length; i++) {
+            for (int j = 0; j < lock.length; j++) {
+                newLock[startLockIdx + i][startLockIdx + j] = lock[i][j];
+            }
+        }
+
+        int rotateCnt = 4;
+
+        while (rotateCnt --> 0) {
+            key = rotate(key);
+            for (int i = 0; i <= endLockIdx; i++) {
+                for (int j = 0; j <= endLockIdx; j++) {
+                    boolean fail = false;
+                    int cnt = 0;
+                    for (int k = 0; k < key.length; k++) {
+                        if (fail) break;
+                        for (int l = 0; l < key.length; l++) {
+                            if ((k + i >= startLockIdx && k + i <= endLockIdx) && (l + j >= startLockIdx && l + j <= endLockIdx)) {
+                                if (key[k][l] == 1) {
+                                    if (newLock[k + i][l + j] == 1) {
+                                        fail = true;
+                                        break;
+                                    } else {
+                                        cnt++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (lockCnt == cnt) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public int[][] rotate(int[][] key) {
+        int[][] copyKey = new int[key.length][key.length];
+
+        for (int i = 0; i < key.length; i++) {
+            for (int j = 0; j < key.length; j++) {
+                copyKey[i][j] = key[key.length - 1 - j][i];
+            }
+        }
+        return copyKey;
     }
 }
