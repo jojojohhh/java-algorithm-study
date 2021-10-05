bfecaa6e34b913be1d95ce92b629a569a7cdcaf3
jojojohhh <qwhtkdwp@gmail.com>
2021-10-05 14:23:38 +0900

    2021/10/05

--- src/main/java/programmers/HighestLowestRankingLottery.java
+++ src/main/java/programmers/HighestLowestRankingLottery.java
@@ -1,8 +1,19 @@
 package programmers;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class HighestLowestRankingLottery {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
-        return answer;
+        Set set = Arrays.stream(win_nums).boxed().collect(Collectors.toSet());
+        int cnt = 0;
+        int zero = 0;
+        for(int lotto: lottos) {
+            if (lotto == 0)
+                zero++;
+            else if (set.contains(lotto))
+                cnt++;
+        }
+        return new int[]{Math.min(lottos.length - (zero + cnt) + 1, 6), Math.min(lottos.length - cnt + 1, 6)};
     }
 }
