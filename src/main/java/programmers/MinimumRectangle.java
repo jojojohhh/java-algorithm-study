490f5ddb97ab2f34cb3b2f9e36780c04cb25d64d
jojojohhh <qwhtkdwp@gmail.com>
2021-10-07 17:35:52 +0900

    2021/10/07

--- src/main/java/programmers/MinimumRectangle.java
+++ src/main/java/programmers/MinimumRectangle.java
@@ -1,8 +1,15 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class MinimumRectangle {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
+        int width = 0, height = 0;
+        for (int[] size: sizes) {
+            Arrays.sort(size);
+            width = Math.max(width, size[0]);
+            height = Math.max(height, size[1]);
+        }
+        return width * height;
     }
 }
