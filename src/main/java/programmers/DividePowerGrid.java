86cbdb2b2b784cc1dc4a2c28ec4256ba4970209e
jojojohhh <qwhtkdwp@gmail.com>
2021-10-07 17:02:10 +0900

    2021/10/07

--- src/main/java/programmers/DividePowerGrid.java
+++ src/main/java/programmers/DividePowerGrid.java
@@ -1,8 +1,56 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DividePowerGrid {
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < wires.length; i++) {
+            ArrayList<Node> nodes = new ArrayList<>();
+            for (int j = 0; j <= n; j++)
+                nodes.add(new Node(j));
+            for (int j = 0; j < wires.length; j++) {
+                if (i != j) {
+                    nodes.get(wires[j][0]).addNode(nodes.get(wires[j][1]));
+                    nodes.get(wires[j][1]).addNode(nodes.get(wires[j][0]));
+                }
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.offer(nodes.get(1));
+            boolean[] visited = new boolean[n + 1];
+            visited[1] = true;
+            int cnt = 1;
+            while (!q.isEmpty()) {
+                 Node node = q.poll();
+                 for (int j = 0; j < node.adjNode.size(); j++) {
+                     Node nextNode = node.adjNode.get(j);
+                     if (!visited[nextNode.n]) {
+                         q.offer(nextNode);
+                         visited[nextNode.n] = true;
+                         cnt++;
+                     }
+                 }
+            }
+            answer = Math.min(answer, Math.abs(n - 2 * cnt));
+        }
+
         return answer;
+    }
+}
+
+class Node {
+    int n;
+    LinkedList<Node> adjNode;
+
+    public Node(int n) {
+        this.n = n;
+        adjNode = new LinkedList<>();
+    }
+
+    public void addNode(Node node) {
+        adjNode.add(node);
     }
 }
--- src/test/java/programmers/DividePowerGridTest.java
+++ src/test/java/programmers/DividePowerGridTest.java
@@ -1,4 +1,24 @@
 package programmers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DividePowerGridTest {
+
+    DividePowerGrid d = new DividePowerGrid();
+
+    @Test
+    public void test1() {
+        Assertions.assertEquals(3, d.solution(9, new int[][]{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}}));
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertEquals(0, d.solution(4, new int[][]{{1, 2}, {2, 3}, {3, 4}}));
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertEquals(1, d.solution(7, new int[][]{{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}}));
+    }
 }
